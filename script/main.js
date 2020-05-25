82f7a209e886ac6713de52302aa3e42c31fa86d7
Taras Senko <taras.senko.99@gmail.com>
2020-05-25 23:04:43 +0300

    Initial commit

---
+++ script/main.js
@@ -0,0 +1,42 @@
+
+const leftMenu = document.querySelector('.left-menu');
+const hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', () => {
+	leftMenu.classList.toggle('openMenu');
+	hamburger.classList.toggle('open');
+});
+
+document.body.addEventListener('click', (e) => {
+	if (!e.target.closest('.left-menu')){
+		leftMenu.classList.remove('openMenu');
+		hamburger.classList.remove('open');
+	}
+});
+
+leftMenu.addEventListener('click', e => {
+	const target = e.target;
+	const dropdown = target.closest('.dropdown');
+	if (dropdown){
+		dropdown.classList.toggle('active');
+		leftMenu.classList.add('openMenu');
+		hamburger.classList.add('open');
+	}
+});
+
+const cartImg = document.querySelectorAll('.tv-card__img');
+
+for (i = 0; i < cartImg.length; i++) {
+	let srcImg = cartImg[i].src;
+	let defaultImg = 'https://i.pinimg.com/originals/4b/1d/11/4b1d11587776cdda3423684ff74f2660.jpg';
+	cartImg[i].addEventListener('mouseover', function (e) {
+		if (e.target.dataset.backdrop != e.target.src && e.target.dataset.backdrop != ''){
+			e.target.src = e.target.dataset.backdrop;
+		} else {
+			e.target.src = defaultImg;
+		}
+	});
+	cartImg[i].addEventListener('mouseout', function (e) {
+		e.target.src = srcImg;
+	});
+}
